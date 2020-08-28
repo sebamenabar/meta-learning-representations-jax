9c27d6fc0ac6a7d85bb3381030edbfac9ab7023d
sebamenabar <samenabar@uc.cl>
2020-08-28 19:16:43 -0400

    code for supervised learning and transfer learning test functions

---
+++ src/test_sup.py
@@ -0,0 +1,47 @@
+import numpy as onp
+
+import jax
+from test_fsl import normalize, lr_fit_eval
+
+
+def forward_loader(pred_fn, loader, device, is_norm=False):
+    all_preds = []
+    all_targets = []
+    for X, y in loader:
+        X = jax.device_put(X, device)
+        preds = pred_fn(X)
+        if is_norm:
+            preds = normalize(preds.reshape(preds.shape[0], -1))
+        all_preds.append(onp.array(preds))
+        all_targets.append(onp.array(y))
+    return onp.concatenate(all_preds), onp.concatenate(all_targets)
+
+
+def test_sup(pred_fn, data_loader, device):
+    all_preds, all_targets = forward_loader(pred_fn, data_loader, device)
+    # all_preds = onp.concatenate(all_preds)
+    # all_targets = onp.concatenate(all_targets)
+
+    return all_preds, all_targets
+
+
+def test_sup_lr(pred_fn, spt_loader, qry_loader, is_norm, device, n_jobs=4):
+    spt_preds, spt_targets = forward_loader(pred_fn, spt_loader, device, is_norm)
+    qry_preds, qry_targets = forward_loader(pred_fn, qry_loader, device, is_norm)
+
+    qry_y_preds = lr_fit_eval(
+        spt_preds.reshape(spt_preds.shape[0], -1),
+        spt_targets,
+        qry_preds.reshape(qry_preds.shape[0], -1),
+        n_jobs=n_jobs,
+    )
+    return qry_y_preds, qry_targets
+
+
+def test_sup_cosine(pred_fn, spt_loader, qry_loader, device):
+    spt_preds, spt_targets = forward_loader(pred_fn, spt_loader, device, True)
+    qry_preds, qry_targets = forward_loader(pred_fn, qry_loader, device, True)
+
+    cosine_distance = spt_preds @ qry_preds.transpose()
+    max_idx = onp.argmax(cosine_distance, axis=0)
+    return spt_targets[max_idx], qry_targets
