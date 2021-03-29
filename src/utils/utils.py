ba5cb3bd8ab99bcefd5d662e1bcfe4f057ad4bbc
sebamenabar <samenabar@uc.cl>
2021-03-29 11:50:15 -0300

    small advances

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,14 @@
 import numpy as onp
-from jax import numpy as jnp, random
+from jax import numpy as jnp, random, tree_util as tree
+
+
+def expand(struct, n, axis=0):
+    return tree.tree_map(
+        lambda x: jnp.broadcast_to(
+            jnp.expand_dims(x, axis), x.shape[:axis] + (n,) + x.shape[axis:]
+        ),
+        struct,
+    )
 
 
 def shuffle_along_axis(rng, a, axis):
@@ -9,14 +18,16 @@ def shuffle_along_axis(rng, a, axis):
 
 def split_rng_or_none(rng, n):
     if rng is None:
-        return jnp.empty((n, 0), dtype=jnp.int32)
+        # return [None] * n
+        return jnp.zeros((n, 1), dtype=jnp.int32)
     else:
         return random.split(rng, n)
 
 
-def use_self_as_default(*top_args):
+def use_self_as_default(*top_args, **are_functions):
     def decorator(func):
         def wrapper(self, *args, **kwargs):
+            args = list(args)
             for arg in top_args:
                 if isinstance(arg, tuple):
                     k0 = arg[0]
@@ -24,13 +35,34 @@ def use_self_as_default(*top_args):
                 else:
                     k0 = k1 = arg
 
-
-                print(arg, func.__code__.co_varnames.index(k0))
-
+                k_index = func.__code__.co_varnames.index(k0)
                 if ((k0 not in kwargs) or (kwargs.get(k0) is None)) and (
-                    len(*args) < (func.__code__.co_varnames.index(k0) - 1)
+                    (len(args) < (k_index))
                 ):
                     kwargs[k0] = getattr(self, k1)
+                elif args[k_index - 1] is None:
+                    args[k_index - 1] = getattr(self, k1)
+
+            for func_name, func_kwargs in are_functions.items():
+                f_index = func.__code__.co_varnames.index(func_name)
+                not_in_kwargs = (func_name not in kwargs) or (
+                    kwargs.get(func_name) is None
+                )
+                args_too_few = len(args) < (f_index)
+                arg_is_none = args_too_few or (args[f_index - 1] is None)
+                if (not_in_kwargs) and (args_too_few or arg_is_none):
+                    _kwargs = {}
+                    for fk in func_kwargs:
+                        if fk in kwargs:
+                            _kwargs[fk] = kwargs[fk]
+                        elif len(args) > func.__code__.co_varnames.index(fk):
+                            _kwargs[fk] = args[func.__code__.co_varnames.index(fk)]
+
+                    if len(_kwargs) == len(func_kwargs):
+                        if not_in_kwargs and args_too_few:
+                            kwargs[func_name] = getattr(self, func_name)(**_kwargs)
+                        elif arg_is_none:
+                            args[f_index - 1] = getattr(self, func_name)(**_kwargs)
             return func(self, *args, **kwargs)
 
         return wrapper
@@ -43,9 +75,16 @@ def call_self_as_default(top_kwargs):
         def wrapper(self, *args, **kwargs):
             for func_name, func_kwargs in top_kwargs.items():
                 if (func_name not in kwargs) or (kwargs.get(func_name) is None):
-                    kwargs[func_name] = getattr(self, func_name)(
-                        **{fk: kwargs[fk] for fk in func_kwargs}
-                    )
+                    _kwargs = {}
+                    for fk in func_kwargs:
+                        if fk in kwargs:
+                            _kwargs[fk] = kwargs[fk]
+                        elif len(args) > func.__code__.co_varnames.index(fk):
+                            _kwargs[fk] = args[func.__code__.co_varnames.index(fk)]
+
+                    if len(_kwargs) == len(func_kwargs):
+                        kwargs[func_name] = getattr(self, func_name)(**_kwargs)
+
             return func(self, *args, **kwargs)
 
         return wrapper
